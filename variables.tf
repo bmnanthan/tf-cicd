88c9db1cb40f3f9a068158d90ab4ab277a820a77
bmnanthan <127930120+bmnanthan@users.noreply.github.com>
2023-03-16 14:07:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,20 +6,20 @@
 
 variable "client_id" {
     description =   "Client ID (APP ID) of the application"
-    type        =   string
+    type        =   9be5b128-a741-4853-afbe-0ef0965af95e
 }
 
 variable "client_secret" {
     description =   "Client Secret (Password) of the application"
-    type        =   string
+    type        =   CjA8Q~4p41lNfGyJJx2qDPKP9kPBCokb_cMHKdk
 }
 
 variable "subscription_id" {
     description =   "Subscription ID"
-    type        =   string
+    type        =   e50c8326-81e7-49c5-be0c-3b81834b5078
 }
 
 variable "tenant_id" {
     description =   "Tenant ID"
-    type        =   string
+    type        =   4b104950-ac33-40b5-a93e-d504b77452ba
 }
